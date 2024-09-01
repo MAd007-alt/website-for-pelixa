ce8cf7d5398b7e47c851bc81944e3a55c14ce00a
MADHUSUDAN V <180014893+MAd007-alt@users.noreply.github.com>
2024-09-01 20:19:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add event listeners to the buttons
+document.getElementById('chatbot-btn').addEventListener('click', () => {
+    // Redirect to the chatbot prototype page
+    window.location.href = 'https://preview.flutlab.io/madhu_sudan/pelixa-chatbot/';
+});
+
+document.getElementById('app-ui-btn').addEventListener('click', () => {
+    // Redirect to the app's user interface page
+    window.location.href = 'https://preview.flutlab.io/blessy_s/pelixa/';
+});
