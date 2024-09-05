9c97cf9f1feb69b9d680794e2995a64abe50561c
MAd007-ctrl <madhuwork94@gmail.com>
2024-09-05 18:54:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Add event listeners to the buttons
 document.getElementById('chatbot-btn').addEventListener('click', () => {
     // Redirect to the chatbot prototype page
-    window.location.href = 'https://preview.flutlab.io/madhu_sudan/pelixa-chatbot/';
+    window.location.href = 'https://pelixa-a7886.web.app/';
 });
 
 document.getElementById('app-ui-btn').addEventListener('click', () => {
